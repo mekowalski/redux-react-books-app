74896166ce8a02ef514277c3a2949f0999b70cdd
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-19 14:46:31 -0600

    added a few more books

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,7 +7,10 @@ const booksReducer = () => {
     { title: 'I Know This Much is True', author: 'Wally Lamb', publishedDate: 1998, genre: 'Psychological Fiction', pages: 912 },
     { title: "Through Grandpa's Eyes", author: 'Patricia MacLachlan', publishedDate: 1980, genre: 'Fiction', pages: 40 },
     { title: 'The Last Night of the Earth Poems', author: 'Charles Bukowski', publishedDate: 1992, genre: 'Poetry', pages: 408 },
-    { title: 'Honey and Milk', author: ' Rupi Kaur', publishedDate: 2014, genre: 'Poetry', pages: 208 }
+    { title: 'Honey and Milk', author: 'Rupi Kaur', publishedDate: 2014, genre: 'Poetry', pages: 208 },
+    { title: '1984', author: 'George Orwell', publishedDate: 1949, genre: 'Science Fiction', pages: 328 },
+    { title: 'The Alchemist', author: 'Paulo Coelho', publishedDate: 1988, genre: 'Drama', pages: 208 },
+    { title: 'Flowers for Algernon', author: 'Daniel Keyes', publishedDate: 1959, genre: 'Science Fiction', pages: 311 }
   ]
 }
 
