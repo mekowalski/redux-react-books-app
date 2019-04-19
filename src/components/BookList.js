81c18301546a5a47b1c4d834e7d02d0a5c0302cc
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-19 12:52:01 -0600

    complelte render method

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,7 +6,14 @@ class BookList extends React.Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <div className='item' key={book.title} >
+        <div className='item' key={book.title}>
+          <div className='right floated content'>
+            <button className='ui button primary'>
+              Select
+            </button>
+          </div>
+
+          <div className='content'>{book.title}</div>
         </div>
       )
     })
