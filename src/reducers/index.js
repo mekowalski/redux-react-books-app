15536ce9b188c8ace884b32d0450275e17597b23
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-19 10:57:16 -0600

    add static list of books

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 //static list of books
 //static arry, no arguments needed, no action because this won't be changed
 const booksReducer = () = {
-
+  return [
+    { title: 'The Martian', author: 'Andy Weir', publishedDate: 2011, genre: 'Science Fiction' pages: 369 },
+    { title: 'Red Sparrow', author: 'Jason Matthews' publishedDate: 2013 genre: 'Spy Thriller' pages: 434 },
+    { title: 'I Know This Much is True' author: 'Wally Lamb' publishedDate: 1998 genre: 'Psychological Fiction' pages: 912 },
+    { title: "Through Grandpa's Eyes" author: 'Patricia MacLachlan' publishedDate: 1980 genre: 'Fiction' pages: 40 },
+    { title: 'The Last Night of the Earth Poems' author: 'Charles Bukowski' publishedDate: 1992 genre: 'Poetry' pages: 408 },
+    { title: 'Honey and Milk' author: ' Rupi Kaur' publishedDate: 2014 genre: 'Poetry' pages: 208 }
+  ]
 }
